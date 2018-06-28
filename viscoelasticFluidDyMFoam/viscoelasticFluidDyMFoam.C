2e6de43cf6fc647396a1d10b8253414ad16bbc11
ascalva <ascalva@gmail.com>
2018-06-28 14:08:37 -0400

    Changed some infile documentation

--- viscoelasticFluidDyMFoam/viscoelasticFluidDyMFoam.C
+++ viscoelasticFluidDyMFoam/viscoelasticFluidDyMFoam.C
@@ -2,33 +2,17 @@
   =========                 |
   \\      /  F ield         | foam-extend: Open Source CFD
    \\    /   O peration     |
-    \\  /    A nd           | For copyright notice see file Copyright
+    \\  /    A nd           |
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-License
-    This file is part of foam-extend.
-
-    foam-extend is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation, either version 3 of the License, or (at your
-    option) any later version.
-
-    foam-extend is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
-
 Application
-    viscoelasticFluidFoam
+    viscoelasticFluidDyMFoam
 
 Description
     Transient solver for incompressible, laminar flow of viscoelastic fluids.
 
 Author
-    Jovani L. Favero and Hrvoje Jasak. All rights reserved
+    Alberto Serrano. All rights reserved
 
 \*---------------------------------------------------------------------------*/
 
